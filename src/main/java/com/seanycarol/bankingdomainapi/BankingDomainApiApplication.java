1441b7e64abc06bb9f4907946fb6109c4f34a087
seanycarol <seany@ice.ufjf.br>
2024-04-21 09:04:14 -0300

    Initial project

---
+++ src/main/java/com/seanycarol/bankingdomainapi/BankingDomainApiApplication.java
@@ -0,0 +1,13 @@
+package com.seanycarol.bankingdomainapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDomainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDomainApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seanycarol/bankingdomainapi/BankingDomainApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanycarol.bankingdomainapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDomainApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
